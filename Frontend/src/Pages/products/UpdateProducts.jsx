b32b32df380ae9db9f782141f8ab696310d3d620
José Alexandre <josealexandre301428@gmail.com>
2024-04-16 10:21:27 -0300

    FEAT: inicia estilização

--- Frontend/src/Pages/products/UpdateProducts.jsx
+++ Frontend/src/Pages/products/UpdateProducts.jsx
@@ -4,6 +4,7 @@ import { validCsv } from '../../services/validateFile';
 
 export default function UpdateProducts() {
     const [selectedFile, setSelectedFile] = useState(null);
+    const [fileNan, setFileNan] = useState(false);
     const [productsOk, setProductsOk] = useState([]);
     const [error, setError] = useState(false);
     const [success, setSuccess] = useState(false);
@@ -13,9 +14,15 @@ export default function UpdateProducts() {
     const handleFileChange = (event) => {
         const file = event.target.files[0];
         setSelectedFile(file);
+        setFileNan(false);
+        setDisable(true)
     };
 
-    const handleButton = async () => {
+    const handleButton = async (file) => {
+        if (!file) {
+            return setFileNan(true)
+        }else {
+            setError(true);
             setErrorProducts([]);
             setError(false);
             setSuccess(false);
@@ -29,7 +36,8 @@ export default function UpdateProducts() {
                 setError(true);
             }else{
                 setDisable(false);
-            }
+            };
+        };
     };
 
     const handleUpload = async () => {
@@ -49,10 +57,36 @@ export default function UpdateProducts() {
 
     return (
         <div>
-            <h2>Upload de Arquivo CSV</h2>
-            <input type="file" accept=".csv" onChange={handleFileChange} /><br />
-            <button onClick={handleButton}>Validar</button> <br />
-            <button disabled={disable} onClick={handleUpload}>Upload</button>
+            <h2 className='text-center'>Upload de Arquivo CSV:</h2>
+            <div className="flex gap-2">
+                <input
+                    id="csvFile"
+                    type="file"
+                    accept=".csv"
+                    className="mt-2 block w-full text-sm file:mr-4 file:rounded-md file:border-0 file:bg-green-700 file:py-2 file:px-4 file:text-sm file:font-semibold file:text-white hover:file:bg-green-800 focus:outline-none disabled:pointer-events-none disabled:opacity-60"
+                    onChange={handleFileChange}
+                />
+                <button 
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => handleButton(selectedFile)}
+                    >
+                    Validar
+                </button>
+                {disable ? (
+                    <button className="bg-red-700 px-4 py-2 rounded-md cursor-not-allowed text-white opacity-50" disabled>
+                        Upload
+                    </button>
+                ) : (
+                    <button
+                        className="bg-green-700 hover:bg-green-800 disable:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={handleUpload}
+                        >
+                        Upload
+                    </button>
+                ) }
+                
+            </div>
+            {fileNan && (<h1>Arquivo não carregado</h1>)}
             {error && (
                 <div>
                     <h3>Erros Encontrados:</h3>
