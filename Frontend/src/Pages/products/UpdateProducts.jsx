cf193ca20b58384443077e87fbc634320c4d21f2
José Alexandre <josealexandre301428@gmail.com>
2024-04-17 18:36:03 -0300

    FEAT: estiliza front, atualiza validaçoes de acordo com os requeisitos do teste

--- Frontend/src/Pages/products/UpdateProducts.jsx
+++ Frontend/src/Pages/products/UpdateProducts.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from "../../services/Api";
-import { validCsv } from '../../services/validateFile';
+import { validCsv, readCsv } from '../../services/validateFile';
 
 export default function UpdateProducts() {
+    const [ base, setBase] = useState([])
     const [selectedFile, setSelectedFile] = useState(null);
     const [fileNan, setFileNan] = useState(false);
     const [productsOk, setProductsOk] = useState([]);
@@ -10,6 +11,16 @@ export default function UpdateProducts() {
     const [success, setSuccess] = useState(false);
     const [errorProducts, setErrorProducts] = useState([]);
     const [disable, setDisable] = useState(true);
+
+
+    const fetch = async () => { 
+        try {
+            const products =  await api.get('/products');
+            setBase(products.data);
+        } catch (error) {
+            setError(true);
+        }
+ };
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
@@ -20,34 +31,38 @@ export default function UpdateProducts() {
 
     const handleButton = async (file) => {
         if (!file) {
-            return setFileNan(true)
-        }else {
-            setError(true);
+            return setFileNan(true);
+        } else {
             setErrorProducts([]);
             setError(false);
             setSuccess(false);
             setProductsOk([]);
-            const products = await api.get('/products');
-            const handleCsv = await validCsv(products, selectedFile);
-            const productsWithErrors = handleCsv.filter((prod) => prod.message);
-
-            if (productsWithErrors.length > 0) {
-                setErrorProducts(productsWithErrors);
-                setError(true);
-            }else{
-                setDisable(false);
-            };
-        };
+            try {
+                const products = await api.get('/products');
+                setBase(products.data)
+                const csvData = await readCsv(file);
+                const validationResult = await validCsv(base, csvData);
+    
+                if (validationResult.errors.length > 0) {
+                    setErrorProducts(validationResult.errors);
+                    setError(true);
+                } else {
+                    const validProducts = validationResult.products;
+                    setProductsOk(validProducts);
+                    console.log(validProducts);
+                    setDisable(false);
+                }
+            } catch (error) {
+                console.error('Erro ao processar arquivo CSV:', error);
+            }
+        }
     };
 
     const handleUpload = async () => {
         if (selectedFile) {
             try {
-                const products = await api.get('/products');
-                const handleCsv = await validCsv(products, selectedFile);
-                const response = await api.put('/upload', handleCsv);
+                const response = await api.put('/upload', productsOk);
                 console.log(response);
-                setProductsOk(handleCsv);
                 setSuccess(true);
             } catch (error) {
                 setError(true);
@@ -55,9 +70,46 @@ export default function UpdateProducts() {
         }
     };
 
+    useEffect(() => {
+        fetch()
+    },[])
+
     return (
         <div>
-            <h2 className='mb-4 mt-4 text-4xl text-center font-extrabold leading-none tracking-tight text-black lg:text-6xl'>Atualização de Produtos</h2>
+            <h1 className='mb-4 mt-4 text-4xl text-center font-extrabold leading-none tracking-tight text-black'>Produtos</h1>
+            <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400" />
+            <div className="py-4 mb-4">
+                
+                <div className="overflow-x-auto container mx-auto mb-20">
+                    <table className="table-auto w-full border-collapse border border-black">
+                        <thead>
+                            <tr className="bg-blue-500">
+                                <th className="px-4 py-2">Código</th>
+                                <th className="px-4 py-2">Nome</th>
+                                <th className="px-4 py-2">Preço de Custo</th>
+                                <th className="px-4 py-2">Preço de Venda</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                           {base.map((prod, index) => {
+                                const {code, name, sales_price, cost_price} = prod;
+                                const cost = Number(cost_price).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                                const sales = Number(sales_price).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+                                return (
+                                    <tr key={index} className="bg-white">
+                                        <td className="border border-black font-bold text-center px-4 py-2">{code}</td>
+                                        <td className="border border-black text-center px-4 py-2">{name}</td>
+                                        <td className="border border-black text-center px-4 py-2">{`R$ ${cost}`}</td>
+                                        <td className="border border-black text-center px-4 py-2">{`R$ ${sales}`}</td>
+                                    </tr>
+                            )})}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <h4 className='mb-4 mt-4 text-4xl text-center font-extrabold leading-none tracking-tight text-black'>Atualização de Produtos</h4>
+            <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400" />
             <div className="flex container mb-3 mx-auto justify-center grid grid-cols-1 gap-1">
                 <label
                     htmlFor="formFile"
@@ -70,78 +122,92 @@ export default function UpdateProducts() {
                     id="formFile"
                     onChange={handleFileChange}/>
             </div>
-            <div className="flex container mx-auto justify-center">
+            <div className="flex container mx-auto justify-center items-stretch mb-20">
                 <button 
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    className="inline-block rounded mr-4 bg-blue-500 text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-blue-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-blue-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0"
                     onClick={() => handleButton(selectedFile)}
                     >
                     Validar
                 </button>
                 {disable ? (
-                    <button className="bg-red-700 px-4 py-2 rounded-md cursor-not-allowed text-white opacity-50" disabled>
-                        Upload
+                    <button className="inline-block rounded border-2 border-red-500 text-red-500 hover:border-red-600 hover:bg-red-400 hover:bg-opacity-10 hover:text-red-600 focus:border-red-700 focus:text-red-700 active:border-red-800 active:text-red-800 dark:border-red-300 dark:text-red-300 dark:hover:hover:bg-red-300 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0" disabled>
+                        Atualizar
                     </button>
                 ) : (
                     <button
-                        className="bg-green-700 hover:bg-green-800 disable:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                        className="inline-block rounded bg-green-500 text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-green-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-green-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-green-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0"
                         onClick={handleUpload}
                         >
-                        Upload
+                        Atualizar
                     </button>
                 ) }
             </div>
-            {fileNan && (<h1>Arquivo não carregado</h1>)}
+            {fileNan && (<h1 className="text-lg text-center font-semibold mb-20 mt-2">Selecione um arquivo!</h1>)}
             {error && (
-                <div>
-                    <h3>Erros Encontrados:</h3>
-                    <table>
+                <div className="py-4">
+                <h3 className="text-lg text-center font-semibold mb-2">Erros Encontrados:</h3>
+                <div className="overflow-x-auto container mx-auto mb-20">
+                    <table className="table-auto w-full border-collapse border border-black">
                         <thead>
-                            <tr>
-                                <th>Código</th>
-                                <th>Nome</th>
-                                <th>Preço de Venda</th>
-                                <th>Novo Preço</th>
-                                <th>Motivo</th>
+                            <tr className="bg-red-500">
+                                <th className="px-4 py-2">Código</th>
+                                <th className="px-4 py-2">Nome</th>
+                                <th className="px-4 py-2">Preço de Venda</th>
+                                <th className="px-4 py-2">Novo Preço</th>
+                                <th className="px-4 py-2">Motivo</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {errorProducts.map((prod, index) => (
-                                <tr key={index}>
-                                    <td>{prod.code}</td>
-                                    <td>{prod.name}</td>
-                                    <td>{prod.sales_price}</td>
-                                    <td>{prod.new_price}</td>
-                                    <td>{prod.message}</td>
+                            {errorProducts.map((prod, index) => {
+                                const {code, name, sales_price, new_price, message} = prod;
+                                const sales = Number(sales_price).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                                const newPrice = Number(new_price).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                                return (
+                                <tr key={index} className="{{ index % 2 === 0 ? 'bg-gray-50' : 'bg-white' }}">
+                                    <td className="border border-black font-bold text-center px-4 py-2">{code}</td>
+                                    <td className="border border-black text-center px-4 py-2">{name}</td>
+                                        <td className="border border-black text-center px-4 py-2">{`R$ ${sales}`}</td>
+                                        <td className="border border-black text-center px-4 py-2">{`R$ ${newPrice}`}</td>
+                                    <td className="border border-black text-center px-4 py-2">{message}</td>
                                 </tr>
-                            ))}
+                            )})}
                         </tbody>
                     </table>
                 </div>
+            </div>
             )}
             {success && (
-                <div>
-                    <h3>Produtos alterados com Sucesso!</h3>
-                    <table>
+                <div className="py-4 mb-20">
+                <h3 className="text-lg text-center font-semibold mb-2">Produtos alterados com Sucesso!</h3>
+                <div className="overflow-x-auto container mx-auto">
+                    <table className="table-auto w-full border-collapse border border-black">
                         <thead>
-                            <tr>
-                                <th>Código</th>
-                                <th>Nome</th>
-                                <th>Preço de Venda</th>
-                                <th>Novo Preço</th>
+                            <tr className="bg-blue-500">
+                                <th className="px-4 py-2">Código</th>
+                                <th className="px-4 py-2">Nome</th>
+                                <th className="px-4 py-2">Preço de Venda</th>
+                                <th className="px-4 py-2">Novo Preço</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {productsOk.map((prod, index) => (
-                                <tr key={index}>
-                                    <td>{prod.code}</td>
-                                    <td>{prod.name}</td>
-                                    <td>{prod.sales_price}</td>
-                                    <td>{prod.new_price}</td>
-                                </tr>
-                            ))}
+                            {productsOk.map((prod, index) => {
+                                const {code, name, sales_price, new_price} = prod;
+                                const sales = Number(sales_price).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                                const newPrice = Number(new_price).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+                                return (
+                                    <tr key={index} className="bg-white">
+                                        <td className="border border-black font-bold text-center px-4 py-2">{code}</td>
+                                        <td className="border border-black text-center px-4 py-2">{name}</td>
+                                        <td className="border border-black text-center px-4 py-2">{`R$ ${sales}`}</td>
+                                        <td className="border border-black text-center px-4 py-2">{`R$ ${newPrice}`}</td>
+                                    </tr>
+                            )})}
                         </tbody>
                     </table>
                 </div>
+            </div>
+            
             )}
         </div>
     );
--- Frontend/src/services/validateFile.js
+++ Frontend/src/services/validateFile.js
@@ -1,6 +1,6 @@
 import Papa from 'papaparse';
 
-const readCsv = (file) => {
+export const readCsv = (file) => {
     return new Promise((resolve, reject) => {
         const csv = [];
         Papa.parse(file, {
@@ -17,12 +17,57 @@ const readCsv = (file) => {
     });
 };
 
-export const validCsv = async (base, file) => {
+const validarCamposNumericos = (produto) => {
+    if (isNaN(Number(produto.new_price))) {
+        throw new Error('Os preços devem ser valores numéricos válidos.');
+    }
+};
+
+const validarProdutoExistente = (baseProdutos, produto) => {
+    if (!baseProdutos.find((baseProduto) => baseProduto.code === produto.product_code)) {
+        throw new Error(`O código do produto '${produto.code}' não existe na base de dados.`);
+    }
+};
+
+const validarPrecosPreenchidos = (produto) => {
+    if (!produto.new_price) {
+        throw new Error('Os preços devem ser preenchidos.');
+    }
+};
+
+export const validCsv = async (baseProdutos, csvData) => {
+    const errors = [];
     const products = [];
-    try {
-        const csv = await readCsv(file); // Espera até que a operação de parsing do CSV seja concluída
-        csv.forEach((csvProduct, index) => {
-            const foundProduct = base.data.find((product) => product.code === csvProduct.product_code);
+
+    if (!csvData || csvData.length === 0) {
+        errors.push({
+            message: "O arquivo CSV está vazio."
+        });
+        return { products, errors };
+    }
+
+    const camposObrigatorios = ['product_code', 'new_price'];
+
+    const headerFields = Object.keys(csvData[0]);
+    for (const campo of camposObrigatorios) {
+        if (!headerFields.includes(campo)) {
+            errors.push({
+                message: `Campo obrigatório '${campo}' não encontrado no arquivo CSV.`
+            });
+        }
+    }
+
+    const baseProdutosMap = new Map(baseProdutos.map(product => [product.code, product]));
+
+    for (let i = 0; i < csvData.length; i++) {
+        const csvProduct = csvData[i];
+
+        try {
+            validarCamposNumericos(csvProduct);
+            validarPrecosPreenchidos(csvProduct);
+
+            const foundProduct = baseProdutosMap.get(csvProduct.product_code);
+
             if (foundProduct) {
                 const baseProductSalesPrice = Number(foundProduct.sales_price);
                 const baseProductCostPrice = Number(foundProduct.cost_price);
@@ -35,24 +80,34 @@ export const validCsv = async (base, file) => {
                 if (biggerThanCost && isValidPriceAdjustment) {
                     products.push({
                         code: foundProduct.code,
-                        name:foundProduct.name,
+                        name: foundProduct.name,
                         sales_price: foundProduct.sales_price,
-                        new_price: csvProduct.new_price });
+                        new_price: csvProduct.new_price
+                    });
                 } else {
-                    products.push({
+                    errors.push({
                         code: foundProduct.code,
-                        name:foundProduct.name,
+                        name: foundProduct.name,
                         sales_price: foundProduct.sales_price,
                         new_price: csvProduct.new_price,
-                        message: `O novo preço não pode ser menor que o custo e deve estar dentro de 10% do valor de venda atual. Erro encontrado na linha: ${index + 1}`
+                        message: `O novo preço não pode ser menor que o custo e deve estar dentro de 10% do valor de venda atual. Erro encontrado na linha: ${i + 1}`
                     });
                 }
             } else {
-                throw new Error(`Produto com código ${csvProduct.product_code}, que consta na linha ${index + 1}, não encontrado na base de dados`);
+                errors.push({
+                    code: csvProduct.product_code,
+                    new_price: csvProduct.new_price,
+                    message: `Produto com código ${csvProduct.product_code}, que consta na linha ${i + 1}, não encontrado na base de dados`
+                });
             }
-        });
-        return products;
-    } catch (error) {
-        throw new Error(error.message); // Rejeita a Promise com o erro em caso de falha na operação de parsing do CSV
+        } catch (error) {
+            errors.push({
+                code: csvProduct.product_code,
+                new_price: csvProduct.new_price,
+                message: `Erro de validação na linha ${i + 1}: ${error.message}`
+            });
+        }
     }
+
+    return { products, errors };
 };
